1b9c62fdd7a38d7e3c65b5473ef66b11454aec96
vanduc2514 <vanduc2514@gmail.com>
2023-02-23 23:22:40 +0700

    wrap main method and format file

--- query.js
+++ query.js
@@ -1,17 +1,19 @@
 import { loadString } from '@logseq/nbb-logseq'
-import { exit } from 'process';
+import { exit } from 'process'
 
 // Expects to be called as `node query.js ...`
-const args = process.argv.slice(2);
-if (args.length < 2) {
-  console.log("Usage: $0 GRAPH-DIR QUERY");
-  exit(1);
-};
-const logseqQuery = logseqQueryFactory(args[0]);
-const jsonPayload = await logseqQuery(args[1]);
-console.log(JSON.stringify(jsonPayload, 0, 2));
+export const queryMain = async () => {
+    const args = process.argv.slice(2)
+    if (args.length < 2) {
+        console.log("Usage: $0 GRAPH-DIR QUERY")
+        exit(1)
+    };
+    const logseqQuery = logseqQueryFactory(args[0])
+    const jsonPayload = await logseqQuery(args[1])
+    console.log(JSON.stringify(jsonPayload, 0, 2))
+}
 
-export default function logseqQueryFactory (logseqBasePath) {
+export default function logseqQueryFactory(logseqBasePath) {
     const template = query => `
         (ns query
         (:require [logseq.graph-parser.cli :as gp-cli]
@@ -25,6 +27,6 @@ export default function logseqQueryFactory (logseqBasePath) {
             (clj->js results)))
         (main "${logseqBasePath}" '${query})
     `
-     // Evaluate cljs inline
-    return async query => await loadString(template(query));
+    // Evaluate cljs inline
+    return async query => await loadString(template(query))
 }
